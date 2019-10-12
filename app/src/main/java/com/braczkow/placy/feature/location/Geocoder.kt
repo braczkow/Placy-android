46129fc6ad8b39043ce8e79e19fad08d7e4b1a4d
Bartosz Raczkowski <bartosz.raczkowski@meelogic.com>
2019-10-12 23:14:45 +0200

    Add geocoder

---
+++ app/src/main/java/com/braczkow/placy/feature/location/Geocoder.kt
@@ -0,0 +1,68 @@
+package com.braczkow.placy.feature.location
+
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import com.braczkow.placy.feature.util.SchedulersFactory
+import com.google.android.gms.maps.model.LatLng
+import io.reactivex.Single
+import java.util.*
+import javax.inject.Inject
+
+typealias GeocodingResult = Pair<Boolean, String?>
+
+interface GeocoderApi {
+    fun getFromLatLng(latLng: LatLng): Single<GeocodingResult>
+
+    companion object {
+        fun makeLatLngStr(latLng: LatLng): String {
+            return String.format(Locale.US, "%.6f, %.6f", latLng.latitude, latLng.longitude)
+        }
+    }
+}
+
+class GeocoderApiImpl @Inject constructor(
+    private val context: Context,
+    private val sf: SchedulersFactory
+) : GeocoderApi {
+    override fun getFromLatLng(latLng: LatLng): Single<GeocodingResult> {
+        return Single.fromCallable {
+            try {
+                val locations = Geocoder(context).getFromLocation(latLng.latitude, latLng.longitude, 1)
+                return@fromCallable if (locations.size > 0) {
+                    GeocodingResult(true, formatAddressString(locations[0]))
+                } else {
+                    GeocodingResult(
+                        false,
+                        GeocoderApi.makeLatLngStr(latLng)
+                    )
+                }
+            } catch (e: Exception) {
+                //Timber.d("Geocoder exception: $e")
+                return@fromCallable GeocodingResult(
+                    false,
+                    GeocoderApi.makeLatLngStr(latLng)
+                )
+            }
+        }.subscribeOn(sf.io())
+    }
+
+    private fun formatAddressString(address: Address): String? {
+        var result: String? = null
+
+        val maxLineIndex = address.maxAddressLineIndex
+        val sb = StringBuilder()
+        for (lineIndex in 0..maxLineIndex) {
+            if (lineIndex != 0) {
+                sb.append(", ")
+            }
+            sb.append(address.getAddressLine(lineIndex))
+        }
+        val addressStr = sb.toString().trim { it <= ' ' }
+        if (addressStr.isNotEmpty()) {
+            result = addressStr
+        }
+
+        return result
+    }
+}
