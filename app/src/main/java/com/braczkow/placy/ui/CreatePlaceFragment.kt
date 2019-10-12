8c019e85b955dcefd14e67e37cd58dcac0a889d1
Bartosz Raczkowski <bartosz.raczkowski@meelogic.com>
2019-10-12 23:15:12 +0200

    CraetePlace fgmt handles User position

--- app/src/main/java/com/braczkow/placy/ui/CreatePlaceFragment.kt
+++ app/src/main/java/com/braczkow/placy/ui/CreatePlaceFragment.kt
@@ -1,19 +1,24 @@
 package com.braczkow.placy.ui
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.braczkow.placy.R
 import com.braczkow.placy.base.App
-import com.braczkow.placy.feature.LocationApi
-import com.braczkow.placy.feature.di.SomeApi
+import com.braczkow.placy.feature.location.LocationApi
+import com.braczkow.placy.feature.location.LocationUpdatesRequest
+import com.braczkow.placy.feature.util.DisposableOnStop
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.Module
 import dagger.Provides
 import dagger.Subcomponent
@@ -44,15 +49,13 @@ class PlaceCreateFragmentModule(private val view: FgmtView) {
 
 class CreatePlaceFragment : Fragment() {
     @Inject
-    lateinit var someApi: SomeApi
-
-    @Inject
     lateinit var locationApi: LocationApi
 
     @Inject
     lateinit var presenter: Presenter
 
     private lateinit var mapFragment: SupportMapFragment
+    private lateinit var locationReq: LocationUpdatesRequest
 
     private val TAG = "PLC " + CreatePlaceFragment::class.java.simpleName
 
@@ -62,8 +65,6 @@ class CreatePlaceFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_create_place, container, false)
-
-        view.place_create_api_txt.setText(someApi.haveFun())
 
         mapFragment =  childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
         mapFragment.getMapAsync { map ->
@@ -75,15 +76,54 @@ class CreatePlaceFragment : Fragment() {
                 moveMarkerTo(latLng)
             }
 
+            map.setOnMarkerClickListener {
+                true
+            }
+
             markerMovementQueue
                 .subscribe { mmr ->
+
+                    map.clear()
+
                     map.animateCamera(CameraUpdateFactory.newLatLngZoom(mmr.latLng, mmr.zoom))
+                    map.addMarker(MarkerOptions().position(mmr.latLng))
                 }
         }
 
-
+        if (ContextCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMS_REQ_CODE)
+        }
 
         return view
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        Log.d(TAG, "onRequestPermissionsResult: $requestCode $permissions $grantResults")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val dos = DisposableOnStop(lifecycle)
+
+        locationReq.start()
+
+        dos.add(locationApi
+            .locationRx()
+            .subscribe {
+                moveMarkerTo(LatLng(it.latitude, it.longitude))
+            })
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        locationReq.stop()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -95,6 +135,8 @@ class CreatePlaceFragment : Fragment() {
             .inject(this)
 
         super.onCreate(savedInstanceState)
+
+        locationReq = locationApi.makeLocationUpdatesRequest()
     }
 
     private fun moveMarkerTo(latLng: LatLng) {
@@ -112,5 +154,6 @@ class CreatePlaceFragment : Fragment() {
 
     companion object {
         private const val DEFAULT_ZOOM = 15.0f
+        private const val PERMS_REQ_CODE = 3001
     }
 }
