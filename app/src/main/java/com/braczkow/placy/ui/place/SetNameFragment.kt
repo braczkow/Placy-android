303e595a712333bbe355b81f936d7dec9aa00279
Bartosz Raczkowski <bartosz.raczkowski@meelogic.com>
2019-10-21 21:58:27 +0200

    SetName fgmt design + extract TextView styles

--- app/src/main/java/com/braczkow/placy/ui/place/SetNameFragment.kt
+++ app/src/main/java/com/braczkow/placy/ui/place/SetNameFragment.kt
@@ -6,21 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 
 import com.braczkow.placy.R
+import timber.log.Timber
 
-/**
- * A simple [Fragment] subclass.
- */
 class SetNameFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_set_name, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        val backCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Timber.d("Handling back button")
+            }
+        }
+
+        requireActivity()
+            .onBackPressedDispatcher
+            .addCallback(backCallback)
+    }
 }
